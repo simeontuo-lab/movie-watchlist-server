4b686528a4881a33b6e8195e89685ab00e3f150d
simeontuo-lab <simeon.tuo@gmail.com>
2026-04-21 13:22:41 -0700

    first commit

---
+++ movies.js
@@ -0,0 +1,12 @@
+const movies = [
+  {title: 'Mario Galaxy', starring: ['Chris Pratt', 'Anya Taylor Joy']},
+  {title: 'The Drama', starring: ['Zendaya', 'Robert Pattinson']},
+  {title: 'Scream 7', starring: ['Neve Campbell', 'Jasmin Savoy Brown']},
+  {title: 'You Me & Tuscany', starring: ['Regé-Jean Page', 'Halle Bailey']},
+  {title: 'Nirvana the Band the Show the Movie', starring: ['Matt Johnson', 'Jay McCarrol']},
+  {title: 'Hamnet', starring: ['Jessie Buckley', 'Paul Mescal']}
+]
+
+export default movies 
+
+ 
---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import movies from './movies.js'
+
+// const express = require('express')
+
+const app = express()
+
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to the movie watchlist server."
+    })
+})
+
+app.get("/movies", (req, res) => {
+    res.json(movies)
+})
+
+app.get("/movies/:name", (req, res) => {
+    res.json(movies.find(movie => movie.title === req.params.name))
+})
+
+const port = 3000
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`)
+})
