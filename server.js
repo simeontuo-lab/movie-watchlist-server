614b2c641fc90755c70b0dcb58f395dd81beffea
simeontuo-lab <simeon.tuo@gmail.com>
2026-04-21 14:13:03 -0700

    4

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import movies from './movies.js'
 
 const app = express()
+app.use(express.json())
+
 
 
 app.get("/", (req, res) => {
@@ -18,7 +20,11 @@ app.get("/movies/:name", (req, res) => {
     res.json(movies.find(movie => movie.title === req.params.name))
 })
 
+app.post("/movies", (req, res) => {
+
 const port = 3000
+
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`)
 })
